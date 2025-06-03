ac6aa25469d9bbb1c7e30e54b1af597dcaa1bceb
PoyoSpooky <150269462+PoyoSpooky@users.noreply.github.com>
2025-06-03 06:57:23 +0000

    update 2

--- src/Controller/PlanetsController.php
+++ src/Controller/PlanetsController.php
@@ -9,6 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/systeme-solaire')]
 class PlanetsController extends AbstractController
 {
+     #[Route('/', name: 'app_planets')]
+    public function index(): Response
+    {
+        return $this->render('planets/index.html.twig');
+    }
+
     #[Route('/soleil', name: 'app_sun')]
     public function sun(): Response
     {
